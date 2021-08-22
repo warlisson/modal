ab3557cdebacd57725e71411566a9bb887f2d74c
Warlisson Miranda <warlisson@ymail.com>
2021-08-22 16:46:34 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,99 +1,97 @@
 <script>
+/**
+ * This class is responsible to
+ * open modals.
+ * 
+ * @package Modal
+ * @version 1.0.0
+ * @author Warlisson Miranda
+ */
 
-		/**
-		 * This class is responsible to
-		 * open modals.
-		 * 
-		 * @package Modal
-		 * @version 1.0.0
-		 * @author Warlisson Miranda
-		 */
+class Modal {
 
-		class Modal {
+	/**
+	 * Construct class
+	 */
 
-			/**
-			 * Construct class
-			 */
+	constructor(config = {}) {
+		this.modal = '.modal';
+		this.openClass = '.open-modal';
+		this.closeClass = '.close-modal';
+		Object.assign(this, config);
+	}
 
-			constructor(config = {}) {
-				this.modal = '.modal';
-				this.openClass = '.open-modal';
-				this.closeClass = '.close-modal';
-				Object.assign(this, config);
-			}
+	/**
+	 * Init events to open or close modal
+	 */
 
-			/**
-			 * Init events to open or close modal
-			 */
-			
-			init() {
+	init() {
 
-				// Constants
-				const obj 			= this;
-				const openBtns 		= document.querySelectorAll(this.openClass);
-				const closeBtns 	= document.querySelectorAll(this.closeClass);
+		// Constants
+		const obj = this;
+		const openBtns = document.querySelectorAll(this.openClass);
+		const closeBtns = document.querySelectorAll(this.closeClass);
 
-				// Open modal
-				openBtns.forEach(openBtn => {
-					openBtn.addEventListener('click', function(event) {
-						event.preventDefault();	
-						obj.open(this);		
-					});
-				});
+		// Open modal
+		openBtns.forEach(openBtn => {
+			openBtn.addEventListener('click', function(event) {
+				event.preventDefault();
+				obj.open(this);
+			});
+		});
 
-				// Close modal
-				closeBtns.forEach(closeBtn => {
-					closeBtn.addEventListener('click', function(event) {
-						event.preventDefault();	
-						obj.close(this);		
-					});
-				});
+		// Close modal
+		closeBtns.forEach(closeBtn => {
+			closeBtn.addEventListener('click', function(event) {
+				event.preventDefault();
+				obj.close(this);
+			});
+		});
 
-			}
+	}
 
-			/**
-			 * Open modal
-			 */
-			
-			open(element) {	
-				let target = this.getModal(element);	
-				let modal = document.querySelector(target);
-				if( modal ){
-					modal.classList.add('open');
-				}				
-			}
+	/**
+	 * Open modal
+	 */
 
-			/**
-			 * Close modal opened
-			 */
-
-			close(element) {
-				let modals = document.querySelectorAll(this.modal);
-				if( modals ){
-					modals.forEach(modal => {
-						if(modal.classList.contains('open')){
-							modal.classList.remove('open');
-						}
-					});
-				}
-			}
-
-			/**
-			 * Get the modal to open
-			 */
-
-			getModal(element){
-				if(element.dataset.modal){
-					return element.dataset.modal;
-				} else if(element.getAttribute('href')){
-					return element.getAttribute('href');
-				} else{				
-					return this.modal;
-				}
-			}
-
+	open(element) {
+		let target = this.getModal(element);
+		let modal = document.querySelector(target);
+		if (modal) {
+			modal.classList.add('open');
 		}
-		
-		(new Modal).init();
-		
-		</script>
+	}
+
+	/**
+	 * Close modal opened
+	 */
+
+	close(element) {
+		let modals = document.querySelectorAll(this.modal);
+		if (modals) {
+			modals.forEach(modal => {
+				if (modal.classList.contains('open')) {
+					modal.classList.remove('open');
+				}
+			});
+		}
+	}
+
+	/**
+	 * Get the modal to open
+	 */
+
+	getModal(element) {
+		if (element.dataset.modal) {
+			return element.dataset.modal;
+		} else if (element.getAttribute('href')) {
+			return element.getAttribute('href');
+		} else {
+			return this.modal;
+		}
+	}
+
+}
+
+(new Modal).init();
+</script>
